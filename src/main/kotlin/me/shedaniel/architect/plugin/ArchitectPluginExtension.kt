dfdcd1b8a2a29d65fa76d6cbd905b7f17eb7e2d8
shedaniel <daniel@shedaniel.me>
2021-02-14 23:15:29 +0800

    Update transformer to 2.0.8

--- src/main/kotlin/me/shedaniel/architect/plugin/ArchitectPluginExtension.kt
+++ src/main/kotlin/me/shedaniel/architect/plugin/ArchitectPluginExtension.kt
@@ -17,7 +17,7 @@ import java.util.jar.JarOutputStream
 import java.util.jar.Manifest
 
 open class ArchitectPluginExtension(val project: Project) {
-    var transformerVersion = "2.0.7"
+    var transformerVersion = "2.0.8"
     var minecraft = ""
     var injectInjectables = true
     private val transforms = mutableMapOf<String, Transform>()
