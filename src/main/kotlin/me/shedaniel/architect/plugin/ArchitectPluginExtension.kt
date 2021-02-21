e9f7d04a670eeb80ed8e2fbd95e4e535436cf718
shedaniel <daniel@shedaniel.me>
2021-02-21 22:17:37 +0800

    Update architectury-plugin to 2.0.20

--- src/main/kotlin/me/shedaniel/architect/plugin/ArchitectPluginExtension.kt
+++ src/main/kotlin/me/shedaniel/architect/plugin/ArchitectPluginExtension.kt
@@ -17,7 +17,7 @@ import java.util.jar.JarOutputStream
 import java.util.jar.Manifest
 
 open class ArchitectPluginExtension(val project: Project) {
-    var transformerVersion = "2.0.19"
+    var transformerVersion = "2.0.20"
     var minecraft = ""
     var injectInjectables = true
     private val transforms = mutableMapOf<String, Transform>()
