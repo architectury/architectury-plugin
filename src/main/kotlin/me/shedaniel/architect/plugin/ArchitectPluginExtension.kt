59ba02bb82fd07adc865bc86278a95de0b618f4a
shedaniel <daniel@shedaniel.me>
2021-03-05 16:27:47 +0800

    Update plugins

--- src/main/kotlin/me/shedaniel/architect/plugin/ArchitectPluginExtension.kt
+++ src/main/kotlin/me/shedaniel/architect/plugin/ArchitectPluginExtension.kt
@@ -18,7 +18,7 @@ import java.util.jar.JarOutputStream
 import java.util.jar.Manifest
 
 open class ArchitectPluginExtension(val project: Project) {
-    var transformerVersion = "2.1.32"
+    var transformerVersion = "2.1.34"
     var injectablesVersion = "1.0.7"
     var minecraft = ""
     var injectInjectables = true
